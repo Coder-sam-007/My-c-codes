be9c888488ff289dc36dd1afbcaa4a96823f072f
Coder-sam-007 <padamzain178@gmail.com>
2022-07-31 23:48:44 +0530

    updated

--- testing.c
+++ testing.c
@@ -1,1 +1,18 @@
-this is a test file
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct testing
+{
+  int a;
+  char name[20];
+};
+
+int main()
+{
+  struct testing t;
+  t.a = 10;
+  t.name[0] = 'a';
+  printf("%d %s\n", t.a, t.name);
+
+  return 0;
+}
